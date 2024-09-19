06afdb79e48e355c0df3e338db8f2cb8cd53685a
Dementev Nikita <embedim@gmail.com>
2024-09-19 12:51:51 +0300

    add default telnet config

--- main/remote_cli.h
+++ main/remote_cli.h
@@ -12,6 +12,7 @@ typedef struct {
 } console_remote_cfg;
 
 void task_tcp_client(void *pvParameters);
+void task_telnet_client(void *pvParameters);
 
 
 #define REMOTE_CLI_DEFAULT() \
@@ -22,6 +23,16 @@ void task_tcp_client(void *pvParameters);
         .c_task_priority = 5,               \
         .task = task_tcp_client,            \
         .port = 7000,                       \
+}
+
+#define REMOTE_TELNET_DEFAULT() \
+{                                           \
+        .s_stack_size = 4096,               \
+        .s_task_priority = 2,               \
+        .c_stack_size = 4096,               \
+        .c_task_priority = 5,               \
+        .task = task_telnet_client,         \
+        .port = 23,                         \
 }
 
 esp_err_t init_remote_cli(console_remote_cfg *cfg);
